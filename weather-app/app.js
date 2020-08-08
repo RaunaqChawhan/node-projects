3221900d4308edc8d1cea3ed97cdd53123c37e93
New <chauhanraunaq878@gmail.com>
2020-08-09 02:00:28 +0530

    Fetch weather data from api

---
+++ weather-app/app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+require('dotenv').config();
+
+const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=40.464,-74.003`;
+
+request({   url: url    }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.current);
+})
