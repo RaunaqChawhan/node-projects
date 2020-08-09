dc1c138f25e29255ee2d39e77d96581c13aaac6c
New <chauhanraunaq878@gmail.com>
2020-08-09 14:17:49 +0530

    Json response using request module

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=40.464,-74.003`;
 
-request({   url: url    }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({   url: url, json: true    }, (error, response) => {
+    // const data = JSON.parse(response.body);
+    console.log(response.body.current);
 })
