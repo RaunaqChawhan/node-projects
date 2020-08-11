fa0b0e52d1b276bb0c969b97fbafcd538a21a206
New <chauhanraunaq878@gmail.com>
2020-08-11 21:17:11 +0530

    Get address from command line

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,22 +2,26 @@ require('dotenv').config();
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Los Angeles', (error, geocodeData) => {
-
-    if (error) {
-        return console.log(error);
-    }
-
-    forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+const address = process.argv[2];
+if (address) {
+    geocode(address, (error, geocodeData) => {
 
         if (error) {
             return console.log(error);
         }
-
-        console.log(geocodeData.location);
-        console.log(forecastData)
-
+    
+        forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+    
+            if (error) {
+                return console.log(error);
+            }
+    
+            console.log(geocodeData.location);
+            console.log(forecastData)
+    
+        });
+    
     });
-
-});
-
+} else {
+    console.log('Please provide a location argument!');
+}
