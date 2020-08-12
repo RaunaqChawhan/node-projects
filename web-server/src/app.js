8fced038a8a43263b2347f109203a9df89cfb1b1
New <chauhanraunaq878@gmail.com>
2020-08-12 23:48:39 +0530

    Display html and json from server on client

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello, root page!');
+    res.send('<h1>Hello, root page!</h1>');
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help page');
+    res.send({
+        msg: 'Express detects an array and an object and stringifies to json to send it to requester'
+    });
 });
 
 app.get('/about', (req, res) => {
