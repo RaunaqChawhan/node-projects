76dfa2c9a9438df1e1c10eb05f29b62fd7fc83a3
New <chauhanraunaq878@gmail.com>
2020-08-14 00:57:23 +0530

    Serve up dynamic content using template engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,28 @@ const express = require('express');
 const app = express();
 const publicDirPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'John'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About App',
+        name: 'John'
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'Help section for the weather app'
+    })
+});
 
 app.get('/weather', (req, res) => {
     res.send('Weather display page');
