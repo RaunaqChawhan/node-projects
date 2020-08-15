2a71bb090b4c8b2cee3ad866ea1dfeddc13c11d6
New <chauhanraunaq878@gmail.com>
2020-08-15 15:59:42 +0530

    Modify default directory and name of views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,18 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirPath = path.join(__dirname, '../public');
 
+// define paths for express config
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// by default, hbs looks for views folder at the root of the project to serve as pages
+// in order to use a different directory and at a location other than root, set the 'views' to new path of that directory
+// setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// setup static directory to serve
 app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
