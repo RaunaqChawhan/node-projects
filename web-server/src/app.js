597c0e4030d469cf16f612c28a5709c2a0bb1a9b
New <chauhanraunaq878@gmail.com>
2020-08-15 17:53:52 +0530

    Configure partials and use across views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,20 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 // define paths for express config
 const publicDirPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // by default, hbs looks for views folder at the root of the project to serve as pages
 // in order to use a different directory and at a location other than root, set the 'views' to new path of that directory
 // setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // setup static directory to serve
 app.use(express.static(publicDirPath));
@@ -32,7 +35,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message: 'Help section for the weather app'
+        title: 'Help Page',
+        message: 'Help section for the weather app',
+        name: 'John'
     })
 });
 
