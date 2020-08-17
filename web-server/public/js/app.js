cadde51198b6c803be7952ec579e8bd61a9a8d61
New <chauhanraunaq878@gmail.com>
2020-08-17 23:30:06 +0530

    Fetch data from client side to server

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Just a check!');
+
+fetch('http://localhost:3000/weather?address=philadelphia')
+.then(res => res.json())
+.then(data => {
+    if (data.error) {
+        console.log(data.error)
+    } else {
+        console.log('Location: ' + data.location);
+        console.log('Forecase: ' + data.forecastData);
+    }
+});
