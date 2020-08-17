843364023079bf064ae03fc710aa8923b4bd434d
New <chauhanraunaq878@gmail.com>
2020-08-18 02:15:35 +0530

    Add location form to get address

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,21 @@
 console.log('Just a check!');
 
-fetch('http://localhost:3000/weather?address=philadelphia')
-.then(res => res.json())
-.then(data => {
-    if (data.error) {
-        console.log(data.error)
-    } else {
-        console.log('Location: ' + data.location);
-        console.log('Forecase: ' + data.forecastData);
-    }
+
+
+const weatherForm = document.querySelector('form');
+const searchedLocation = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch(`http://localhost:3000/weather?address=${searchedLocation.value}`)    
+    .then(res => res.json())
+    .then(data => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log('Location: ' + data.location);
+            console.log('Forecase: ' + data.forecastData);
+        }
+    });
 });
